288ccaf24f2ab793e5581062c6c1ec76563bc688
Roman Walch <walch@taceo.io>
2024-06-11 10:56:50 +0200

    feat: add send/recv for view

--- src/nccl/safe.rs
+++ src/nccl/safe.rs
@@ -243,6 +243,41 @@ impl Comm {
         }
     }
 
+    pub fn send_view<T: NcclType>(
+        &self,
+        data: &CudaView<T>,
+        peer: i32,
+    ) -> Result<(), result::NcclError> {
+        unsafe {
+            result::send(
+                data.ptr as *mut _,
+                data.len,
+                T::as_nccl_type(),
+                peer,
+                self.comm.0,
+                self.device.stream as *mut _,
+            )?;
+        }
+        Ok(())
+    }
+
+    pub fn recv_view<T: NcclType>(
+        &self,
+        buff: &mut CudaView<T>,
+        peer: i32,
+    ) -> Result<result::NcclStatus, result::NcclError> {
+        unsafe {
+            result::recv(
+                buff.ptr as *mut _,
+                buff.len,
+                T::as_nccl_type(),
+                peer,
+                self.comm.0,
+                self.device.stream as *mut _,
+            )
+        }
+    }
+
     pub fn broadcast<T: NcclType>(
         &self,
         sendbuff: &Option<&CudaSlice<T>>,
